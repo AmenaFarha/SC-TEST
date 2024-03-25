d099bd0227cc23905f44384b7cb290e20978c88b
AmenaFarha <amena15-11845@diu.edu.bd>
2024-03-25 13:15:39 -0400

    account3

---
+++ code2.py
@@ -0,0 +1,6 @@
+# This script calculates the area of a circle
+import math
+
+radius = 5
+area = math.pi * radius ** 2
+print("Area of the circle:", area)
---
+++ code5.py
@@ -0,0 +1,3 @@
+# This script generates a list of squares of numbers from 1 to 10
+squares = [i ** 2 for i in range(1, 11)]
+print(squares)
