282fef5a98a1bb4fd9c19aad0c2b4c7856a302b5
AmenaFarha <amena15-11845@diu.edu.bd>
2024-03-25 13:12:29 -0400

    account3

---
+++ code7.py
@@ -0,0 +1,6 @@
+# This script uses a dictionary to count the occurrence of each character in a string
+from collections import Counter
+
+my_string = "hello world"
+counter = Counter(my_string)
+print(counter)
